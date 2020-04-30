bf861d13ad2af5d0c152136f238ab16f07a45dae
Enterpryz <enterpryz1@gmail.com>
2020-04-29 20:21:32 -0400

    Fantastic Mr. Fox

---
+++ Assets/Scripts/AttackerS/Attacker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 5f)]
+    float fltCurrentSpeed = 1f;
+    GameObject GOcurrentTarget;
+    
+    void Update()
+    {
+        // speed to move attacker
+        transform.Translate(Vector2.left * fltCurrentSpeed * Time.deltaTime);
+        UpdateAnimationState();
+    } // Update()
+
+    private void UpdateAnimationState()
+    {
+        if (!GOcurrentTarget)
+        {
+            // stop attacking once the defender is destroyed
+            GetComponent<Animator>().SetBool("IsAttacking", false);
+        }
+    } // UpdateAnimationState()
+
+    public void SetMovementSpeed(float speed)
+    {
+        fltCurrentSpeed = speed;
+    } // SetMovementSpeed()
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("IsAttacking", true);
+        GOcurrentTarget = target;
+    } // Attack()
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!GOcurrentTarget)
+        {
+            return;
+        }
+        Health health = GOcurrentTarget.GetComponent<Health>();
+
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
+    } // StrikeCurrentTarget()
+
+} // class Attacker
+
---
+++ Assets/Scripts/AttackerS/Fox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject GOotherObject = otherCollider.gameObject;
+
+        if (GOotherObject.GetComponent<Gravestone>())
+        {
+            GetComponent<Animator>().SetTrigger("JumpTrigger");
+        }
+
+        else if (GOotherObject.GetComponent<Defender>()) ;
+        {
+            GetComponent<Attacker>().Attack(GOotherObject);
+        }
+    }
+
+
+    
+
+
+
+
+}
